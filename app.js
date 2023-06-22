42f020551d7c14be7d27f2932b6d64b43ba615bc
Abheekrishna <abheekrishna17@gmail.com>
2023-06-22 18:53:56 +0530

    Finished calc

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const numberBtn = document.querySelectorAll('[data-numbers]');
-const operationBtn = document.querySelectorAll('[data-operation]');
+const operationBtn = document.querySelectorAll('[data-operations]');
 const equalBtn = document.querySelector('[data-equals]');
 const allClearBtn = document.querySelector('[data-allClear]');
 const deleteBtn = document.querySelector('[data-delete]');
@@ -22,4 +22,59 @@ numberBtn.forEach(number => {
         currentNumber +=  e.target.innerText;
         currentNumberTextDiv.innerText = currentNumber;
     })
+})
+
+operationBtn.forEach(operation => {
+    operation.addEventListener('click', (e) => {
+        if(!currentNumber) return;
+        haveDot = false;
+        const operationName = e.target.innerText;
+        if(currentNumber && previousNumber && lastOperation) {
+            mathOperation();
+        } else {
+            result = currentNumber;
+        }
+        lastOperation = operationName;
+        clear(lastOperation);
+    })
+})
+
+const mathOperation = () => {
+    if(lastOperation === '+') {
+        result = parseFloat(result) + parseFloat(currentNumber);
+    } else if(lastOperation === '-') {
+        result = parseFloat(result) - parseFloat(currentNumber);
+    } else if(lastOperation === '*') {
+        result = parseFloat(result) * parseFloat(currentNumber);
+    } else if(lastOperation === '/') {
+        result = parseFloat(result) / parseFloat(currentNumber);
+    }
+}
+
+const clear = (name = '') => {
+    previousNumber = `${currentNumber} ${name}`;
+    previousNumberTextDiv.innerText = previousNumber;
+    currentNumber = '';
+    currentNumberTextDiv.innerText = '';
+}
+
+equalBtn.addEventListener('click', () => {
+    if(!currentNumber || !previousNumber) return;
+    haveDot = false;
+    mathOperation();
+    clear(lastOperation);
+    currentNumberTextDiv.innerText = result;
+})
+
+allClearBtn.addEventListener('click', () => {
+    currentNumber = '';
+    currentNumberTextDiv.innerText = '';
+    previousNumber = '';
+    previousNumberTextDiv.innerText = '';
+    lastOperation = '';
+});
+
+deleteBtn.addEventListener('click', () => {
+    currentNumber = currentNumber.toString().slice(0, -1);
+    currentNumberTextDiv.innerText = currentNumber;
 })
